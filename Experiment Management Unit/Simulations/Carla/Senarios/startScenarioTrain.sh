91e593d85ee0e7de1617630c8a3702dfad20a8f8
ArielMobileLab <teleoperator.study@gmail.com>
2023-11-19 16:14:55 +0200

    change

---
+++ Experiment Management Unit/Simulations/Carla/Senarios/startScenarioTrain.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+killall -9 python
+killall -9 CarlaUE4 CarlaUE4.sh || echo "CarlaUE4 was not running."
+
+
+# Function to perform cleanup on Ctrl+C
+function cleanup() {
+    echo "Received Ctrl+C. Cleaning up..."
+    
+    # Add any cleanup actions here
+    ( killall -9 CarlaUE4 CarlaUE4.sh 2>&1 ) > /dev/null
+
+    pkill -f "roslaunch carla_ros_bridge carla_ros_bridge_with_example_ego_vehicle.launch"
+    pkill -f "$run_python $HOME/cognata_sdk_ros1_ws/src/cognata_sdk/src/N_FaceFunction19.9.py"
+    pkill -f "$run_python $HOME/Desktop/CARLA_0.9.13/PythonAPI/examples/Map_objects/Train/cars.py"
+    pkill -f "$run_python" "$HOME/Desktop/CARLA_0.9.13/speed/speed.py"
+    
+    # Exit the script
+    exit 1
+}
+
+# Trap Ctrl+C and call the cleanup function
+trap cleanup INT
+
+
+# Function to free the port of the server of carla
+function free_port() {
+    local port_to_free=$1
+    local process_id=$(fuser $port_to_free/tcp 2>/dev/null)
+    
+    if [ -n "$process_id" ]; then
+        echo "Process $process_id is using port $port_to_free. Terminating..."
+        kill -9 $process_id
+        echo "Process $process_id terminated."
+    else
+        echo "Port $port_to_free is not in use."
+    fi
+}
+
+# Free port 2000
+free_port 2000
+
+# Kill existing Python and Carla processes
+killall -9 python
+( killall -9 CarlaUE4 CarlaUE4.sh 2>&1 ) > /dev/null
+echo "Killed existing Python and Carla processes."
+
+clear
+
+
+run_python=python3
+town=Town03;
+
+case $1 in
+    Town01Restaurant)
+        scenario=Town01Restaurant
+        town=Town01
+        ;;
+    Town03GasStation)
+        scenario=Town03GasStation
+        town=Town03
+        ;;
+    Town03TrainTrack)
+        scenario=Town03TrainTrack
+        town=Town03
+        ;;
+    Town04ParkingLot)
+        scenario=Town04ParkingLot
+        town=Town04
+        ;;
+    Town07Farm)
+        scenario=Town07Farm
+        town=Town07
+        ;;
+    *)
+        scenario=Town03GasStation
+        town=Town03
+        ;;
+
+
+esac
+/home/omer/Desktop/CARLA_0.9.13/Carla Logs
+"$HOME/Desktop/CARLA_0.9.13/CarlaUE4.sh" "/Game/Carla/Maps/Town05" > log_server.txt &
+
+sleep 6s
+xdotool windowminimize "$(wmctrl -l | grep "CarlaUE4" | cut -d ' ' -f1)"
+ sleep 6s
+ source /home/omer/carla-ros-bridge/catkin_ws/devel/setup.bash &&
+
+ # $1 give the first argumnet that responsible for the town chose as we able to see in the agrumnet of the lunch file
+
+
+roslaunch carla_ros_bridge Train.launch town:=$1&
+
+
+ # $2 give the argumnet that responsible for the Face Agent 
+ if [ "$2" == "Face" ] ||  [ "$2" = "Color" ] ]; then
+ "$run_python" "$HOME/cognata_sdk_ros1_ws/src/cognata_sdk/src/N_FaceFunction19.9.py" $2&
+ fi
+
+ "$run_python" "$HOME/Desktop/API Addapter/Addapter.py" &
+ "$run_python" "$HOME/Desktop/CARLA_0.9.13/speed/speed.py"&
+
+ # $3 give the argumnet that responsible for the objects of the senario 
+ if [ "$3" == "Train" ]; then
+ sleep 6s &&
+  run_python=python #this script run with python and not python 3 like the face script
+ "$run_python" "$HOME/Desktop/Autonomous Resope Unit/Arrow_Guide/Arrows_guide.py" $4 &
+ "$run_python" "$HOME/Desktop/Carla_Logs/Ego_Car_log.py" &
+ "$run_python" "$HOME/Desktop/CARLA_0.9.13/PythonAPI/examples/Map_objects/Train/cars.py" 
+ 
+ fi
+
+killall -9 python
+( killall -9 CarlaUE4 CarlaUE4.sh 2>&1 ) > /dev/null
+
--- Experiment Management Unit/Simulations/Cognata/Main_Menu.py
+++ Experiment Management Unit/Simulations/Cognata/Main_Menu.py
@@ -1,163 +1,224 @@
-#!/usr/bin/env python
-
-
 import subprocess
-from cognata_api.web_api.cognata_demo import CognataRequests as cog_api
-import time
 import os
-import threading
-import queue
-import timeit
-import signal
-from contextlib import contextmanager
-import sys
-import json
-import keyboard
-import rospy
 import tkinter as tk
-from tkinter import messagebox
-from geometry_msgs.msg import Point
-from std_msgs.msg import Float64
-from sensor_msgs.msg import NavSatFix
-from tkinter import ttk
+from tkinter import messagebox, ttk
 from PIL import Image, ImageTk
 import vlc
-import subprocess
 import webbrowser
 
-
-import os
-
-def play_video(video_path):
-    # video_x = 1920 * 2  # Adjust this value to match the screen width of your setup
-    # video_y = 0        # Adjust this value to match the screen height and positioning of your setup
-    subprocess.Popen(['vlc',video_path,'--fullscreen','--play-and-exit']) 
-
-
-
-# Create the root window
 root = tk.Tk()
 s = ttk.Style()
 root.title("MobileLab Scenario Menu")
 root.geometry("1920x1080")
 
-# Use the Clam theme
 s.theme_use('clam')
-s.configure("Custom.TButton", padding=20, font=("Tahoma", 12))
+s.configure("Custom.TButton", padding=12, font=("Tahoma", 12))
+s.configure("Smaller.TButton", padding=13, font=("Tahoma", 12))
 
 files = os.listdir("/dev/input/by-id/")
-
 wheelName = 'null'
-# Iterate over the list of files
+
 for file in files:
-    # Check if the file name contains the string "Wheel-event-joystick"
     if "-event-joystick" in file:
         wheelName = file
 
+def play_video(video_path):
+    subprocess.Popen(['vlc', video_path, '--fullscreen', '--play-and-exit']) 
 
-# Create a function to launch the selected scenario
-def launch_scenario(scenarioslist,Town,Face,Objects, Arrow_Guilde):
+def launch_scenario(scenarioslist):
     if wheelName != 'null':
         os.system('ffset /dev/input/by-id/' + wheelName + ' -a 30')
+    
     for scenario in scenarioslist:
-        root.withdraw()  # added this line to hide the root window
-        repeat = 1
-        while repeat == 1:
-            try:
-
-                # Use subprocess to execute the command
-                subprocess.run(["bash", scenario, Town, Face, Objects, Arrow_Guilde], check=True)
-                repeat = 0
-            except subprocess.CalledProcessError:
-                repeat = 1
-
-        if repeat == 0:
-            root.deiconify()
-            root.update()
-
+        root.withdraw()
+        for command in scenario:
+            repeat = True
+            while repeat:
+                os.system(command)
+                if subprocess.call(["pgrep", "-f", "SimCloud"]) == 0: #If you detect an exit from cognata
+                    repeat = messagebox.askyesno("Warning", "Repeat?") 
+                    if not repeat:
+                        root.update()
+                        break  # Exit the while loop if repeat is False
+                    root.update()
+                else: #If cognata simulator failed to load up break the loop
+                    break    
 
 
+        root.deiconify()
+        root.update()
+                    
 
+# def reload(x):
 
-# Create a list of scenarios
+#     if wheelName != 'null':
+#         os.system('ffset /dev/input/by-id/' + wheelName + ' -a 30')
+#     repeat = 1
+#     while repeat == 1:
+#         os.system(x)
+#         if 'kill -9 $(ps -e | grep SimCloud | head -c 6)':
+#             repeat = messagebox.askyesno("Warning", "Repeat?")
+#         if repeat == 0:
+#             root.update()
+#             break
+#         root.update()
+#         root.withdraw()  # added this line to hide the root windowsa
 
 scenarios = [
     {
         "name": "Training",
         "description": "Get used to driving with our steering wheel and brakes in an urban scenario",
-        "video_path": "./videos/Training.mp4", 
-        "code": ["/home/omer/Desktop/CARLA_0.9.13/Main_menu/startScenarioTrain.sh","Town05","No_Face","Train","Guide_Train"]
+        "video_path": "./videos/Training.mp4",
+        "buttons_names": ["Go to Scenario"],
+        "code": [[
+            "python3 adaptiveTraining.py train CarControl_0_0 allocentric-rear"
+            ]]
     },
     {
-        "name": "Face",
-        "description": "Drive around the city whole following the instruction signs with Face Agent ",
-        "video_path": "./videos/Face.mp4",
-        "code": ["/home/omer/Desktop/CARLA_0.9.13/Main_menu/startScenarioSpatial.sh","Town01","Face","Spatial","Guide_Spatial"]
+        "name": "Load-Car1",
+        "description": "In this scenario you will experience communication time delay while driving after a lead vehicle (black car) - think you can teleoperate safely",
+        "video_path": "./videos/ACC semantic.mp4",
+        "buttons_names": ["50ms ML", "150ms ML", "50ms 15m", "150ms 15m", "250ms 15m", "250ms ML"],
+        "code": [     
+            #1     
+            ["python3 adaptiveTraining.py load-1 CarControl_50_Matlab allocentric-rear"],
+            #2
+            ["python3 adaptiveTraining.py load-1 CarControl_150_Matlab allocentric-rear"],
+            #3
+            ["python3 adaptiveTraining.py load-1 CarControl_50_15 allocentric-rear"],
+            #4
+            ["python3 adaptiveTraining.py load-1 CarControl_150_15 allocentric-rear"], 
+	    #5 
+	    ["python3 adaptiveTraining.py load-1 CarControl_250_15 allocentric-rear"], 	 
+            #6
+	    ["python3 adaptiveTraining.py load-1 CarControl_250_Matlab allocentric-rear"] 	                      
+            ]
+    },
+{
+        "name": "Load-Car2",
+        "description": "In this scenario you will experience communication time delay while driving after a lead vehicle (black car) - think you can teleoperate safely",
+        "video_path": "./videos/ACC semantic.mp4",
+        "buttons_names": ["50ms ML", "150ms ML", "50ms 15m", "150ms 15m", "250ms 15m", "250ms ML"],
+        "code": [     
+            #1     
+            ["python3 adaptiveTraining.py load-2 CarControl_50_Matlab allocentric-rear"],
+            #2
+            ["python3 adaptiveTraining.py load-2 CarControl_150_Matlab allocentric-rear"],
+            #3
+            ["python3 adaptiveTraining.py load-2 CarControl_50_15 allocentric-rear"],
+            #4
+            ["python3 adaptiveTraining.py load-2 CarControl_150_15 allocentric-rear"],  
+            #5 
+	    ["python3 adaptiveTraining.py load-2 CarControl_250_15 allocentric-rear"], 	 
+            #6
+	    ["python3 adaptiveTraining.py load-2 CarControl_250_Matlab allocentric-rear"] 	                                                        
+            ]
+    },
+{
+        "name": "Load-Car3",
+        "description": "In this scenario you will experience communication time delay while driving after a lead vehicle (black car) - think you can teleoperate safely",
+        "video_path": "./videos/ACC semantic.mp4",
+        "buttons_names": ["50ms ML", "150ms ML", "50ms 15m", "150ms 15m", "250ms 15m", "250ms ML"],
+        "code": [     
+            #1     
+            ["python3 adaptiveTraining.py load-3 CarControl_50_Matlab allocentric-rear"],
+            #2
+            ["python3 adaptiveTraining.py load-3 CarControl_150_Matlab allocentric-rear"],
+            #3
+            ["python3 adaptiveTraining.py load-3 CarControl_50_15 allocentric-rear"],
+            #4
+            ["python3 adaptiveTraining.py load-3 CarControl_150_15 allocentric-rear"],
+            #5 
+	    ["python3 adaptiveTraining.py load-3 CarControl_250_15 allocentric-rear"], 	 
+            #6
+	    ["python3 adaptiveTraining.py load-3 CarControl_250_Matlab allocentric-rear"]                                     
+            ]
     },
     {
-        "name": "Color",
-        "description": "Drive around the city whole following the instruction signs with Color Agent",
-        "video_path": "./videos/Color.mp4",
-        "code": ["/home/omer/Desktop/CARLA_0.9.13/Main_menu/startScenarioSpatial.sh","Town01","Color","Spatial","Guide_Spatial"]
+        "name": "Load-Cube",
+        "description": "To ease communication time delay, the teleoperation system, uses semantic representation of cars. In addition to the time delay,  you will see bounding boxes instead of cars. Think you can handle this one",
+        "video_path": "./videos/ACC semantic.mp4",
+        "buttons_names": ["50ms ML", "150ms ML", "50ms 15m", "150ms 15m", "250ms 15m", "250ms ML"],
+        "code": [#1
+                ["python3 adaptiveTraining.py load2_cube CarControl_50_Matlab allocentric-rear"],
+                 #2
+                 ["python3 adaptiveTraining.py load2_cube CarControl_150_Matlab allocentric-rear"],
+                 #3
+                 ["python3 adaptiveTraining.py load2_cube CarControl_50_15 allocentric-rear"],
+                 #4
+                 ["python3 adaptiveTraining.py load2_cube CarControl_150_15 allocentric-rear"],
+                 #5 
+	         ["python3 adaptiveTraining.py load2_cube CarControl_250_15 allocentric-rear"], 	 
+                 #6
+	         ["python3 adaptiveTraining.py load2_cube CarControl_250_Matlab allocentric-rear"]    
+]
     },
     {
-        "name": "No_Face",
-        "description": "Drive around the city whole following the instruction signs",
-        "video_path": "./videos/None.mp4",
-        "code": ["/home/omer/Desktop/CARLA_0.9.13/Main_menu/startScenarioSpatial.sh","Town01","No_Face","Spatial","Guide_Spatial"]
+        "name": "Spatial",
+        "description": "Drive around the city while  following the instruction signs Use regular driver viewpoint",
+        "video_path": "./videos/Allocentric.mp4",
+        "buttons_names": ["Allo", "Ego"],
+        "code": [
+                 #ALO
+                 ["python3 adaptiveTraining.py spatial CarControl_0_0 allocentric"],
+                 #EGO
+                 ["python3 adaptiveTraining.py spatial CarControl_0_0 egocentric"]]
     }
+   
+
 ]
 
 
 
-# Create a function to display the scenario menu
 def show_scenario_menu():
-    # Create a label with the big title
-    big_title_label = tk.Label(root, text="Welcome to MobileLab Scenario Menu", font=("Tahoma", 38, "bold"))
+    big_title_label = tk.Label(root, text="Welcome to MobileLab Scenario Menu", font=("Tahoma", 20, "bold"))
     big_title_label.pack(pady=20)
 
-    # Create a label with the smaller title
-    small_title_label = tk.Label(root, text="Please choose a scenario", font=("Tahoma", 31))
+    small_title_label = tk.Label(root, text="Please choose a scenario", font=("Tahoma", 20))
     small_title_label.pack()
 
-    # Create buttons for each scenario
     for scenario in scenarios:
-        # Create a frame to hold the scenario information
         scenario_frame = tk.Frame(root, bg="black", padx=10, pady=10)
         scenario_frame.pack(pady=15, anchor="w", fill="x")
 
-        # Create a scenario button
-        scenario_button = ttk.Button(scenario_frame, text="Go to Scenario", command=lambda s=scenario: launch_scenario(s["code"] ,s["code"][1],s["code"][2],s["code"][3],s["code"][4]))
-        #scenario_button = ttk.Button(scenario_frame, text="Go to Scenario", command=lambda s=scenario: launch_scenario(s["code"],s["Town"]))
-        scenario_button.configure(style="Custom.TButton")
-        scenario_button.pack(side=tk.RIGHT)
-
-        # Create a frame for the description label
         description_frame = tk.Frame(scenario_frame, bg="black")
         description_frame.pack(side=tk.LEFT, padx=10, fill="both", expand=True)
 
-        # Create a label with the scenario description
-        title_label = tk.Label(description_frame, text=scenario["name"], font=("Tahoma", 16, "bold"), wraplength=300,width=28, fg="white", bg="black",anchor="w")
+        title_label = tk.Label(description_frame, text=scenario["name"], font=("Tahoma", 16, "bold"), wraplength=300, width=28, fg="white", bg="black", anchor="w")
         title_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        
+        if "description" in scenario:
+            description_label = tk.Label(description_frame, text=scenario["description"], font=("Tahoma", 15), width=70, wraplength=780, fg="white", bg="black")
+            description_label.grid(row=0, column=1, columnspan=2, padx=1, pady=1)
 
-        # Create a label with the scenario description
-        description_label = tk.Label(description_frame, text=scenario["description"], font=("Tahoma", 13),width=70, wraplength=780, fg="white", bg="black")
-        description_label.grid(row=0, column=1,columnspan=2, padx=1, pady=1)
-
-        # Configure the grid columns to center-align the labels
         title_label.columnconfigure(0, weight=1)
         description_frame.columnconfigure(1, weight=1)
 
-        # Create a play video button
+        custom_style = ttk.Style()
+        custom_style.configure("Custom.TButton", font=("Helvetica", 8), padding=7)
+
         play_video_button = ttk.Button(scenario_frame, text="Play Video", command=lambda url=scenario["video_path"]: play_video(url))
         play_video_button.configure(style="Custom.TButton")
         play_video_button.pack(side=tk.RIGHT, padx=5)
 
-# Show the initial scenario menu
-show_scenario_menu()
+     
 
-# Run the main loop
+        buttons_frame = tk.Frame(scenario_frame, bg="black")
+        buttons_frame.pack(side=tk.RIGHT)
+
+        num_buttons = len(scenario["buttons_names"])
+        num_rows = (num_buttons + 1) // 2  # Calculate number of rows (rounded up)
+
+        for i, button_name in enumerate(scenario["buttons_names"]):
+            index = i  # Index variable to track the current index in the code array
+            scenario_button = ttk.Button(buttons_frame, text=button_name, command=lambda s=scenario, i=index: launch_scenario([s["code"][i]]))
+            scenario_button.configure(style="Custom.TButton")
+            row = i // 2  # Calculate row index
+            col = i % 2   # Calculate column index
+            scenario_button.grid(row=row, column=col, padx=5, pady=5)
+
+
+
+show_scenario_menu()
 root.mainloop()
 
-
-# OnLy GoD cAn judGe mE
