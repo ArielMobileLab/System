73e20ae5bede2f03efbe19532e6c60de0db3105a
ArielMobileLab <76939624+ArielMobileLab@users.noreply.github.com>
2024-02-04 11:44:07 +0200

    Update Avatar_Agent.py

--- Autonomous Response Unit/Avatar_Agent/Avatar_Agent.py
+++ Autonomous Response Unit/Avatar_Agent/Avatar_Agent.py
@@ -16,6 +16,10 @@ from collections import OrderedDict
 from datetime import datetime
 import json
 import pygame
+from carla_msgs.msg import CarlaCollisionEvent
+from nav_msgs.msg import Odometry
+
+
 
 
 
@@ -23,14 +27,15 @@ Agent_type = sys.argv[1]
 
 #Agent_type = 'Face'
 #Agent_type = 'Color'
+#Agent_type = 'Face_Familiar'
 
 
 
 pygame.init()
 
-sound_face_1 = pygame.mixer.Sound("/home/omer/Desktop/sound/happy.mp3")
-sound_face_2 = pygame.mixer.Sound("/home/omer/Desktop/sound/sad.mp3")
-sound_face_3 = pygame.mixer.Sound("/home/omer/Desktop/sound/angry.mp3")
+#sound_face_1 = pygame.mixer.Sound("/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/sound/happy.mp3")
+sound_face_2 = pygame.mixer.Sound("/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/sound/1.wav")
+sound_face_3 = pygame.mixer.Sound("/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/sound/2.wav")
 
 
 processing = False
@@ -44,30 +49,30 @@ Exit = 1
 Stress_Model = 1
 Mean = 1
 timestamp = 0
+write_to_json_flag = True  
 
-desktop_path = os.path.expanduser('~/Desktop/Carla_Logs')
-
+folder_path = "/home/omer/Desktop/Carla_Logs/Logs"
 current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+file_name = os.path.join(folder_path, 'Face_Status{}_{}.json'.format(Agent_type, current_time))
 
-file_name = os.path.join(desktop_path, 'Face_Status_{}.json'.format(current_time))
 
+if  Agent_type == 'Face' or Agent_type == 'Face_Train':
+    Image_1='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/FaceImage/1.jpeg'
+    Image_2='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/FaceImage/2.jpeg'
+    Image_3='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/FaceImage/3.jpeg'
 
-if Agent_type=='Face':
-    Image_1='/home/omer/Desktop/FaceImage/1.jpeg'
-    Image_1_1='/home/omer/Desktop/FaceImage/1.1.jpeg'
-    Image_2='/home/omer/Desktop/FaceImage/2.jpeg'
-    Image_2_2='/home/omer/Desktop/FaceImage/2.2.jpeg'
-    Image_3='/home/omer/Desktop/FaceImage/3.jpeg'
-    Image_3_3='/home/omer/Desktop/FaceImage/3.2.jpeg'
 
 
 if Agent_type=='Color':
-    Image_1='/home/omer/Desktop/ColorImage/green1.jpeg'
-    Image_1_1='/home/omer/Desktop/ColorImage/green1.1.jpeg'
-    Image_2='/home/omer/Desktop/ColorImage/orange1.jpeg'
-    Image_2_2='/home/omer/Desktop/ColorImage/orange1.1.jpeg'
-    Image_3='/home/omer/Desktop/ColorImage/red1.jpeg'
-    Image_3_3='/home/omer/Desktop/ColorImage/red1.1.jpeg'
+    Image_1='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/ColorImage/green1.jpeg'
+    Image_2='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/ColorImage/green1.jpeg'
+    Image_3='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/ColorImage/red1.jpeg'
+
+
+if Agent_type=='Face_Familiar':
+    Image_1='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/FaceImageFamiliar/1.jpeg'
+    Image_2='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/FaceImageFamiliar/2.jpeg'
+    Image_3='/home/omer/Desktop/Autonomous Resope Unit/Avatar_Guide/FaceImageFamiliar/3.jpeg'
 
 
 
@@ -77,7 +82,6 @@ def write_to_json(data_dict):
     with open(file_name, 'a') as json_file:
         json.dump(data_dict, json_file)
         json_file.write('\n')  # Add a newline for readability
-
 
 
 
@@ -95,39 +99,57 @@ def callback(msg):
         sys.exit()
 
 
+def collision_callback(data):
 
+    global write_to_json_flag
+    write_to_json_flag = False
+   
+
+
+def my_shutdown_callback():
+    
+    global write_to_json_flag
+    write_to_json_flag = False
 
 
 def GetStress(msg):
 
-
    global Final_value_Array
    global Mean
-   global Stress_Model
+   global Stress_Model	
 
-   Final_value = math.sqrt(msg.linear_acceleration.x**2 + msg.linear_acceleration.y**2)
+
+   Final_value = math.sqrt(msg.linear_acceleration.x**2 + msg.linear_acceleration.y**2*0.8)
+   #Final_value = math.sqrt(msg.linear_acceleration.x**2 + msg.linear_acceleration.y**2*1.3 + msg.linear_acceleration.y**2)
    #Final_value = math.sqrt(msg.linear_acceleration.x**2 + msg.linear_acceleration.y**2 + msg.linear_acceleration.z**2)
    Final_value_Array.append(Final_value)
    Final_value_Array[:] = Final_value_Array[-20:]  # Keep only the last 20 elements
    Mean =  np.average(Final_value_Array)
-   Stress_Model = math.exp(0.0513 + 0.142 * math.log(Mean))
 
-
+   epsilon = 1e-10  # A small positive value to prevent issues with log(0)
+   if Mean > epsilon:
+    Stress_Model = math.exp(0.0513 + 0.142 * math.log(Mean))
+   else:
+    # Handle the case when Mean is too close to zero
+    Stress_Model = 0  # You can choose an appropriate default value
 
 
 
 def Imege(photo_path, sec,simulation_time):
 
+    global write_to_json_flag
      
-    timestamp = time.time() * 1000 
-
+    timestamp = datetime.now().strftime('%H:%M:%S.%f')
+    
     Egocar_data = OrderedDict()
     Egocar_data["Type"] = "Face_Status:"
     Egocar_data["Timestamp"] = timestamp
     Egocar_data["Simulation_time"] = simulation_time
     Egocar_data["Face_State"] = Face_State
-
-    write_to_json(Egocar_data) # for json
+    
+    if write_to_json_flag:
+       write_to_json(Egocar_data) # for json
+       
 
     plt.rcParams['toolbar'] = 'None'
     # create a figure and subplot
@@ -158,8 +180,12 @@ def Imege(photo_path, sec,simulation_time):
         print("0")
 
 
-
-
+def odometry_callback(data):
+	 global write_to_json_flag
+	 if -116.1248 < data.pose.pose.position.y < -90.128 and 88.2840 < data.pose.pose.position.x < 92.3620:
+		write_to_json_flag = False
+  
+	
 
 def AgentPlot(msg):
   
@@ -192,35 +218,34 @@ def AgentPlot(msg):
                 print(" close")
                 sys.exit()
 
-
    if not processing:
 
- 
         processing = True
-
     
         # to start with image number one (2 sec)
-        if Start:
-            Face_State = 1
-            pygame.mixer.Sound.play(sound_face_1)
-            photo_path = Image_1
-            sec = 2
-            Imege(photo_path, sec,simulation_time)
-            Start = False
+        #if Start:
+        #    Face_State = 1
+        #    pygame.mixer.Sound.play(sound_face_2)
+        #    photo_path = Image_2
+        #    sec = 2
+        #    Imege(photo_path, sec,simulation_time)
+        #    Start = False
 
-        elif Face_State == 1:
-            if Stress_Model > 1:
+        if Face_State == 1:
+            #if Stress_Model > 1:
+            if Stress_Model > 0.8:    
                 Face_State = 2
-                pygame.mixer.Sound.play(sound_face_2)
+                #pygame.mixer.Sound.play(sound_face_2)
                 plt.close('all')
-                sec = 4
+                sec = 2
                 photo_path = Image_2
                 Imege(photo_path, sec,simulation_time)
 
             # Add more conditions as needed
 
         elif Face_State == 2:
-            if Stress_Model > 1.3:
+            #if Stress_Model > 1.23:
+            if Stress_Model > 1.275:
                 Face_State = 3
                 pygame.mixer.Sound.play(sound_face_3)
                 plt.close('all')
@@ -228,35 +253,30 @@ def AgentPlot(msg):
                 photo_path = Image_3
                 Imege(photo_path, sec,simulation_time)
 
-            elif Stress_Model < 0.7 and Face_State != 3:
-                pygame.mixer.Sound.play(sound_face_1)
-                plt.close('all')
-                Face_State = 1
-                photo_path = Image_1
-                sec = 1
-                Imege(photo_path, sec, simulation_time)
+            #elif Stress_Model < 0.7 and Face_State != 3:
+            #    pygame.mixer.Sound.play(sound_face_1)
+            #    plt.close('all')
+            #    Face_State = 1
+            #    photo_path = Image_1
+            #    sec = 1
+            #    Imege(photo_path, sec, simulation_time)
 
             # Add more conditions as needed
 
         elif Face_State == 3:
             
-            if Stress_Model < 1.2:
-                pygame.mixer.Sound.play(sound_face_2)
+            #if Stress_Model < 1.18:
+            if Stress_Model < 1.175:
+                #pygame.mixer.Sound.play(sound_face_2)
                 Face_State = 2
                 plt.close('all')
                 after_hard_face = True
-                sec = 4
+                sec = 2
                 photo_path = Image_2
                 Imege(photo_path, sec,simulation_time)
 
-            # Add more conditions as needed
-
-        # Continue with the rest of your code...
-
 
         processing = False
-
-
 
 
 # Initialize ROS node
@@ -267,11 +287,14 @@ matplotlib.use("TkAgg")
 # Subscribe to the topic that publishes the messages from Carla
 rospy.Subscriber("IMU_topic", Imu, GetStress,queue_size=1)
 rospy.Subscriber("GPS_topic",NavSatFix,AgentPlot,queue_size=1)
+rospy.Subscriber("Collision_topic", CarlaCollisionEvent, collision_callback)
+rospy.Subscriber("Odometry_topic", Odometry, odometry_callback)
 
 rospy.Subscriber("/rosout", Log, callback)  # Subscribe to '/rosout' topic
 # Set the desired loop frequency (1 Hz in this example)
 rate = rospy.Rate(1)
 
+rospy.on_shutdown(my_shutdown_callback)
 
 # Continue running the code
 while not rospy.is_shutdown():
@@ -284,4 +307,3 @@ while not rospy.is_shutdown():
 
 
    rate.sleep()
-
