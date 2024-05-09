51a98c462ff9ee895e54ae4c127c68ff3631b319
ArielMobileLab <76939624+ArielMobileLab@users.noreply.github.com>
2024-05-09 10:51:39 +0300

    Add files via upload

---
+++ Experiment Management Unit/Simulations/Carla/Corraction_expiriment/Main_Menu.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+
+import subprocess
+from cognata_api.web_api.cognata_demo import CognataRequests as cog_api
+import time
+import os
+import threading
+import queue
+import timeit
+import signal
+from contextlib import contextmanager
+import sys
+import json
+import keyboard
+import rospy
+import tkinter as tk
+from tkinter import messagebox
+from geometry_msgs.msg import Point
+from std_msgs.msg import Float64
+from sensor_msgs.msg import NavSatFix
+from tkinter import ttk
+from PIL import Image, ImageTk
+import vlc
+import subprocess
+import webbrowser
+
+
+import os
+
+def play_video(video_path):
+    
+    # video_x = 1920 * 2  # Adjust this value to match the screen width of your setup
+    # video_y = 0        # Adjust this value to match the screen height and positioning of your setup
+    subprocess.Popen(['vlc',video_path,'--fullscreen','--play-and-exit']) 
+
+def open_url(url):
+    webbrowser.open(url)
+
+
+
+# Create the root window
+root = tk.Tk()
+s = ttk.Style()
+root.title("MobileLab Scenario Menu")
+root.geometry("1920x1080")
+
+# Use the Clam theme
+s.theme_use('clam')
+s.configure("Custom.TButton", padding=20, font=("Tahoma", 12))
+
+files = os.listdir("/dev/input/by-id/")
+
+wheelName = 'null'
+# Iterate over the list of files
+for file in files:
+    # Check if the file name contains the string "Wheel-event-joystick"
+    if "-event-joystick" in file:
+        wheelName = file
+
+
+# Create a function to launch the selected scenario
+def launch_scenario(scenarioslist,Town,Face,Objects, Arrow_Guilde):
+    if wheelName != 'null':
+        os.system('ffset /dev/input/by-id/' + wheelName + ' -a 50')
+    for scenario in scenarioslist:
+        root.withdraw()  # added this line to hide the root window
+        repeat = 1
+        while repeat == 1:
+            try:
+
+                # Use subprocess to execute the command
+                subprocess.run(["bash", scenario, Town, Face, Objects, Arrow_Guilde], check=True)
+                repeat = 0
+            except subprocess.CalledProcessError:
+                repeat = 1
+
+        if repeat == 0:
+            root.deiconify()
+            root.update()
+
+
+
+
+
+# Create a list of scenarios
+
+scenarios = [
+    {
+        "name": "Car_in_lane",
+        "description": "",
+        "video_path": "/home/omer/Desktop/Experiment Managment Unit/Simulations/Main_Menu Videos/Face.mov",
+        "NASA1": "https://docs.google.com/forms/d/e/1FAIpQLSfx4wrdAquDLUB-hKgwHPeI9ikIynPzvf-pvr3FSruK1RQASA/viewform",
+	"NASA2": "https://docs.google.com/forms/d/e/1FAIpQLSfGy6BIwAGpHZDJ-Uk6T1q9awHvASKNafpMJdrHi8HLvBCeOg/viewform",
+        "code": ["/home/omer/Desktop/Experiment Managment Unit/Simulations/Carla/Corraction/Senarios/Car_In_Lane.sh","Town04","High_Way","Obstacle_A","Guide_Spatial"],
+        "code1": ["/home/omer/Desktop/Experiment Managment Unit/Simulations/Carla/Corraction/Senarios/Car_In_Lane.sh","Town04","High_Way","Obstacle_B","Guide_Spatial"],
+
+    },
+    {
+        "name": "Training",
+        "description": "",
+        "video_path": "/home/omer/Desktop/Experiment Managment Unit/Simulations/Main_Menu Videos/Training.mov", 
+        "NASA": "https://docs.google.com/forms/d/e/1FAIpQLSfGy6BIwAGpHZDJ-Uk6T1q9awHvASKNafpMJdrHi8HLvBCeOg/viewform",
+        "code": ["/home/omer/Desktop/Experiment Managment Unit/Simulations/Carla/Corraction/Senarios/newtrain.sh","Town02","Face_Train","Nah","Guide_Train"]
+    },
+    {
+        "name": "extra_senario",
+        "description": "",
+        "video_path": "/home/omer/Desktop/Experiment Managment Unit/Simulations/Main_Menu Videos/Training.mov", 
+        "NASA": "https://docs.google.com/forms/d/e/1FAIpQLSfGy6BIwAGpHZDJ-Uk6T1q9awHvASKNafpMJdrHi8HLvBCeOg/viewform",
+        "code": ["/home/omer/Desktop/Experiment Managment Unit/Simulations/Carla/Corraction/Senarios/startScenarioFirstResponders.sh","Town04","Face_Train","Nah","First_Response"]
+    },
+]
+
+
+
+# Create a function to display the scenario menu
+def show_scenario_menu():
+    # Create a label with the big title
+    big_title_label = tk.Label(root, text="Welcome to MobileLab Scenario Menu", font=("Tahoma", 38, "bold"))
+    big_title_label.pack(pady=20)
+
+    # Create a label with the smaller title
+    small_title_label = tk.Label(root, text="Please choose a scenario", font=("Tahoma", 31))
+    small_title_label.pack()
+
+   
+    # Create buttons for each scenario
+    for scenario in scenarios:
+	    
+	
+        # Create a frame to hold the scenario information
+        scenario_frame = tk.Frame(root, bg="black", padx=10, pady=10)
+        scenario_frame.pack(pady=15, anchor="w", fill="x")
+
+        if scenario["name"] == "Training":
+            # Create a scenario button
+            scenario_button = ttk.Button(scenario_frame, text="Scenario", command=lambda s=scenario: launch_scenario(s["code"] ,s["code"][1],s["code"][2],s["code"][3],s["code"][4]))
+            #scenario_button = ttk.Button(scenario_frame, text="Go to Scenario", command=lambda s=scenario: launch_scenario(s["code"],s["Town"]))
+            scenario_button.configure(style="Custom.TButton")
+            scenario_button.pack(side=tk.RIGHT)
+        else:   
+            # Create a scenario button
+            scenario_button = ttk.Button(scenario_frame, text="Scenario A", command=lambda s=scenario: launch_scenario(s["code"] ,s["code"][1],s["code"][2],s["code"][3],s["code"][4]))
+            #scenario_button = ttk.Button(scenario_frame, text="Go to Scenario", command=lambda s=scenario: launch_scenario(s["code"],s["Town"]))
+            scenario_button.configure(style="Custom.TButton")
+            scenario_button.pack(side=tk.RIGHT)
+    
+
+        # Create a frame for the description label
+        description_frame = tk.Frame(scenario_frame, bg="black")
+        description_frame.pack(side=tk.LEFT, padx=10, fill="both", expand=True)
+
+        s.configure("Danger.TButton", foreground="Black", background="green", padding=(20, 15), font=("Tahoma", 14, "bold"))     
+
+       
+
+        # Create a label with the scenario description
+        title_label = tk.Label(description_frame, text=scenario["name"], font=("Tahoma", 16, "bold"), wraplength=300,width=28, fg="white", bg="black",anchor="w")
+        title_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+        # Create a label with the scenario description
+        description_label = tk.Label(description_frame, text=scenario["description"], font=("Tahoma", 13),width=70, wraplength=780, fg="white", bg="black")
+        description_label.grid(row=0, column=1,columnspan=2, padx=1, pady=1)
+
+        # Configure the grid columns to center-align the labels
+        title_label.columnconfigure(0, weight=1)
+        description_frame.columnconfigure(1, weight=1)
+
+        # Create a play video button
+        play_video_button = ttk.Button(scenario_frame, text="Video", command=lambda url=scenario["video_path"]: play_video(url))
+        play_video_button.configure(style="Custom.TButton")
+        play_video_button.pack(side=tk.RIGHT, padx=5)
+
+
+    frame = tk.Frame(root, bg="black", padx=10, pady=10)
+    frame.pack(pady=15, anchor="w", fill="x")
+
+
+
+
+# Show the initial scenario menu
+show_scenario_menu()
+
+# Run the main loop
+root.mainloop()
+
+
+# OnLy GoD cAn judGe mE
---
+++ Experiment Management Unit/Simulations/Carla/Corraction_expiriment/Senarios/Car_In_Lane.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+function cleanup() {
+    echo "Received Ctrl+C. Cleaning up..."
+
+    pkill -TERM -f "CarlaUE4.sh"
+    sleep 2s
+
+    pkill -TERM -f "carla_ros_bridge"
+    sleep 5s
+    pkill -KILL -f "carla_ros_bridge" || echo "carla_ros_bridge process terminated successfully."
+    
+    rosnode kill /speedometer
+
+    killall -9 python
+    sleep 2s
+    killall -9 python3
+
+    exit 1
+}
+
+function free_port() {
+    local port_to_free=2000
+    local process_id=$(fuser $port_to_free/tcp 2>/dev/null)
+    
+    if [ -n "$process_id" ]; then
+        echo "Process $process_id is using port $port_to_free. Terminating..."
+        kill -9 $process_id
+        echo "Process $process_id terminated."
+    else
+        echo "Port $port_to_free is not in use."
+    fi
+}
+
+free_port
+
+trap cleanup INT
+
+"$HOME/Desktop/CARLA_0.9.13/CarlaUE4.sh" -windowed -ResX=640 -ResY=480 -benchmark -fps=10 &
+
+sleep 11s
+
+source /home/omer/carla-ros-bridge/catkin_ws/devel/setup.bash &&
+
+roslaunch carla_ros_bridge Coraction.launch town:=$1&
+
+sleep 3s
+
+run_python=python3
+
+
+"$run_python" "$HOME/Desktop/API Addapter/Addapter.py" &
+"$run_python" "$HOME/Desktop/CARLA_0.9.13/speed/speed.py" &
+"$run_python" "$HOME/Desktop/API Addapter/RosToUDP.py" &
+
+
+sleep 6s &&
+
+run_python=python
+
+#"$run_python" "$HOME/Desktop/Autonomous Resope Unit/Arrow_Guide/Arrows_guide.py" $4 &
+
+#"$run_python" "$HOME/Desktop/CARLA_0.9.13/PythonAPI/examples/Map_objects/Coraction/Corraction_Objects.py" &
+#"$run_python" "/home/omer/Desktop/Ego Car Logs/Carla logs/Objects_log.py" $2 &
+#"$run_python" "/home/omer/Desktop/Ego Car Logs/Carla logs/Ego_Car_log.py" $2 &#sleep 2s &&
+
+
+sleep 1s &&
+
+run_python=python
+
+#"$run_python" "$HOME/Desktop/Autonomous Resope Unit/Arrow_Guide/Arrows_guide.py" $4 &
+"$run_python" "$HOME/Desktop/Autonomous Resope Unit/RearVIew/Front_Camera_API.py" $4 &
+#"$run_python" "$HOME/Desktop/Autonomous Resope Unit/RearVIew/Rear_camera_API.py" $4 &
+sleep 1s &&
+
+"$run_python" "$HOME/Desktop/Carla_Logs/Ego_Car_Log_Coraction.py" $2 &
+"$run_python" "$HOME/Desktop/Carla_Logs/Objects_Car_in_lane.py" $2 &
+
+
+#"$run_python" "$HOME/Desktop/CARLA_0.9.13/PythonAPI/examples/Map_objects/Coraction/Car_Lane.py" &
+
+
+
+"$run_python" "$HOME/Desktop/API Addapter/phy.py" 
+
+wait
+
+free_port
+
+cleanup
