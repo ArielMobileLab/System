884a9d5235ca95262fda2e6695faa4405be14ba0
ArielMobileLab <76939624+ArielMobileLab@users.noreply.github.com>
2024-11-04 14:01:41 +0200

    Update Speed.py

--- Autonomous Response Unit/Speedometer/Speed.py
+++ Autonomous Response Unit/Speedometer/Speed.py
@@ -1,35 +1,100 @@
-
+#!/usr/bin/env python
+# Speed meter for all scripts
+import rospy
+from sensor_msgs.msg import NavSatFix
 import os
+import sys
+from PyQt5.QtGui import QGuiApplication, QScreen
+from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication
 import signal
-import subprocess
-speed_process_id = None
-
-def run_speedometer():
-        
-        global speed_process_id
-        # os.system(
-        #    'terminator --geometry=340x130-2700+1000 -T speed -p speed -x rosrun cognata_sdk #speed.py')  # Will die when run thread dies (daemon)
-        cmd = ["rosrun", "cognata_sdk", "speed_carla.py"]
-		
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=set_death_signal(), )
-        #save the process id of the speed
-        speed_process_id = proc.pid 
-        print("hi")
-    # END OF LAUNCHSCENARIO CLASS
-
-#close the speed when death signal is send
-def set_death_signal():
-    def _set_death_signal():
-        os.prctl(os.PR_SET_PDEATHSIG, signal.SIGTERM)
+#from carla_msgs.msg import CarlaEgoVehicleStatus
+from std_msgs.msg import Float32, Int8, Float64, Float32
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication
 
 
+rospy.init_node("speedometer")
 
-# Define a function to send a termination signal to the speed process
-def terminate_speed_process(process_id):
-    try:
-        # Send a SIGTERM signal to the process
-        os.kill(process_id, signal.SIGTERM)
-    except ProcessLookupError:
-        pass  # Process may have already exited
 
-run_speedometer()
+class fonts:  # works only for print() function
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+
+
+def callback(msg):
+
+    #speed = msg.velocity
+    #speed = msg.altitude
+    #print(msg)
+    #print(speed)
+    
+    #window.ui.widget.updateValue(round(speed * 3.6, 1))
+    window.ui.widget.updateValue(msg.data* 3.6)
+    # print(fonts.RED + str(round(speed * 3.6, 1)) + " km/h")
+
+
+################################################################################################
+# Convert UI to PyQt5 py file
+################################################################################################
+os.system("pyuic5 -o analoggaugewidget_demo_ui.py analoggaugewidget_demo.ui")
+# os.system("pyuic5 -o analoggaugewidget_demo_ui.py analoggaugewidget_demo.ui.oQCkCR")
+
+################################################################################################
+# Import the generated UI
+################################################################################################
+from analoggaugewidget_demo_ui import *
+
+
+################################################################################################
+# MAIN WINDOW CLASS
+################################################################################################
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self)
+        ################################################################################################
+        # Setup the UI main window
+        ################################################################################################
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+        #self.ui.widget.setStyleSheet("background-color: #F5F5F5;")  # Show window on second screen at the bottom left
+        ################################################################################################
+        screen = QGuiApplication.screens()[3]  # secound screen
+        screen_geometry = screen.geometry()
+        window_width, window_height = 300, 300  # dimensions of the window
+        x = screen_geometry.center().x() - window_width / 2  # x-coordinate of the top-left corner at the bottom middle
+        y = screen_geometry.bottom() - window_height  # y-coordinate of the top-left corner at the bottom of the screen
+        self.setGeometry(x, y, window_width, window_height)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+	    
+        ################################################################################################
+        # Show window
+        ################################################################################################
+        self.show()
+
+
+def signal_handler(signal, frame):
+    # Close the window
+    window.close()
+    # Exit the application
+    sys.exit(0)
+
+#Get a signal form Adaptive traning to close the speedometer
+signal.signal(signal.SIGUSR1, signal_handler)
+sub = rospy.Subscriber("/carla/ego_vehicle/speedometer", Float32, callback, queue_size=10)
+#sub =rospy.Subscriber("/carla/ego_vehicle/gnss",NavSatFix,GPS,queue_size=1)
+
+app = QApplication(sys.argv)
+########################################################################
+##
+########################################################################
+window = MainWindow()
+window.show()
+sys.exit(app.exec_())
+
+rospy.spin()
