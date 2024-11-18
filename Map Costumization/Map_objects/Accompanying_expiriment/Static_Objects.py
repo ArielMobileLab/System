353c1d5ccc5233f02abfd84bc7c6ba841b7ba438
ArielMobileLab <76939624+ArielMobileLab@users.noreply.github.com>
2024-11-18 15:16:42 +0200

    Update Static_Objects.py

--- Map Costumization/Map_objects/Accompanying_expiriment/Static_Objects.py
+++ Map Costumization/Map_objects/Accompanying_expiriment/Static_Objects.py
@@ -6,14 +6,6 @@ client = carla.Client('localhost', 2000)
 # Retrieve the CARLA world
 world = client.get_world()
 
-actors = world.get_actors()
-for actor in actors:
-    if actor.type_id.startswith("traffic.traffic_light"):
-        traffic_light = actor
-        # Change the traffic light state to green (Assuming the state is "Green")
-        traffic_light.set_state(carla.TrafficLightState.Green)
-        traffic_light.set_green_time(10000)  # Optional: Set the green time duration
-
     # Create a WeatherParameters object
 weather = carla.WeatherParameters(
 cloudiness=0.0,
@@ -32,10 +24,7 @@ world.set_weather(weather)
 
     # Create a traffic cone
 blueprint_library = world.get_blueprint_library()
-cone_bp1 = blueprint_library.filter("static.prop.streetbarrier")[0] 
-cone_location1 = carla.Location(x=245.699997, y=327, z=0.1)
-cone_transform1 = carla.Transform(cone_location1, carla.Rotation(pitch=0, yaw=90.0, roll=0))
-cone1 = world.spawn_actor(cone_bp1, cone_transform1)
+
 
 warning = blueprint_library.filter("static.prop.streetbarrier")[0] 
 warning_location = carla.Location(x=245.699997, y=323, z=0.1)
@@ -43,7 +32,8 @@ warning_transform = carla.Transform(warning_location, carla.Rotation(pitch=0, ya
 warning = world.spawn_actor(warning, warning_transform)
 
 
-cone_car_location = carla.Location(x=242, y=326.750061, z=0.300000)
+cone_car_location = carla.Location(x=255, y=326.0, z=0.300000)
 cone_car_bp = blueprint_library.filter("vehicle.dodge.charger_police")[0]
 cone_car_transform = carla.Transform(cone_car_location, carla.Rotation(pitch=0, yaw=180, roll=0))
 cone_car = world.spawn_actor(cone_car_bp, cone_car_transform)
+
