4d70fcf6cb461a2cca8d3e45f813242ac8539bd0
ArielMobileLab <76939624+ArielMobileLab@users.noreply.github.com>
2025-01-12 13:38:02 +0200

    Update RosToUDP.py

--- API Addapter Unit/RosToUDP.py
+++ API Addapter Unit/RosToUDP.py
@@ -3,43 +3,59 @@ import struct
 import rospy
 import carla
 from sensor_msgs.msg import NavSatFix
+import os
+import sys
 
-# Define two IP addresses
 MatLabIP = "10.20.0.188"
-GUI_tele_assist_IP = "10.20.0.184"
-Port = 12355
+MatLabPort = 12355
 bufferSize = 2048
 
-# Create the UDP socket
+Senario_Type = sys.argv[1]
+Senario_Type = int(Senario_Type)
+
+
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-# Connect to Carla
 client = carla.Client('localhost', 2000)
 client.set_timeout(10.0)
 world = client.get_world()
 
 simulation_time_ros = 0.0
 
+# Initialize a global variable
+
 def on_world_tick(world_snapshot):
+
     global simulation_time_ros
+
     simulation_time = world_snapshot.timestamp.elapsed_seconds
-    time = [simulation_time_ros, simulation_time]
 
-    # Pack the data
-    packed_data = struct.pack("%dd" % len(time), *time)
+    data_Send = [simulation_time_ros, simulation_time, Senario_Type]
 
-    # Send the packed data to both IP addresses
-    UDPServerSocket.sendto(packed_data, (MatLabIP, Port))
-    UDPServerSocket.sendto(packed_data, (GUI_tele_assist_IP, Port))
+    # Pack the data using the format string
+    packed_data = struct.pack("%dd" % len(data_Send), *data_Send)
+
+
+    # Send the packed data to the specified IP and port
+    UDPServerSocket.sendto(packed_data, (MatLabIP, MatLabPort))
+
+
 
 def simulation_time_UDP(data):
+
     global simulation_time_ros
+
+    # Ros time
     simulation_time_ros = data.header.seq * 0.033333335071821
+
 
 def main():
     rospy.init_node('RosToUDP')
+    
     world.on_tick(lambda snapshot: on_world_tick(snapshot))
+    # Subscribe to the GNSS topic
     rospy.Subscriber("/carla/ego_vehicle/gnss", NavSatFix, simulation_time_UDP)
+    
     rospy.spin()
 
 if __name__ == '__main__':
