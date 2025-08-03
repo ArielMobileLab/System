7c76d6116fc367f601734a7622ba79cce400d9ac
ArielMobileLab <76939624+ArielMobileLab@users.noreply.github.com>
2025-08-03 11:06:50 +0300

    Update Vocal_Avatar.py

--- Autonomous Response Unit/Avatar_Main_Project/Voice Version 11LAB/Vocal_Avatar.py
+++ Autonomous Response Unit/Avatar_Main_Project/Voice Version 11LAB/Vocal_Avatar.py
@@ -3,7 +3,7 @@ from elevenlabs import play
 
 # Initialize the ElevenLabs client
 elevenlabs = ElevenLabs(
-    api_key="sk_4e8ef70dd79e8504a48a73f2d8df205e2d38afcbf3b44432"
+    api_key="Need to add key"
 )
 
 
@@ -11,9 +11,9 @@ elevenlabs = ElevenLabs(
 
 # Hebrew TTS example
 audio = elevenlabs.text_to_speech.convert(
-    text="red!!! light!!! STOP!!! ",  # Hebrew text
-    voice_id="TxGEqnHWrfWFTfGW9XjX",  # Choose a voice that works for you
-    model_id="eleven_monolingual_v1",  # Recommended for Hebrew
+    text="red!!! light!!! STOP!!! ",  
+    voice_id="TxGEqnHWrfWFTfGW9XjX",  
+    model_id="eleven_monolingual_v1",  
     output_format="mp3_44100_128",
     voice_settings={
         "stability": 0.3,
@@ -26,9 +26,9 @@ audio = elevenlabs.text_to_speech.convert(
 play(audio)
 
 audio = elevenlabs.text_to_speech.convert(
-    text="red!!! light!!! STOP!!!",  # Hebrew text
-    voice_id="TxGEqnHWrfWFTfGW9XjX",  # Choose a voice that works for you
-    model_id="eleven_monolingual_v1",  # Recommended for Hebrew
+    text="red!!! light!!! STOP!!!",  
+    voice_id="TxGEqnHWrfWFTfGW9XjX",  
+    model_id="eleven_monolingual_v1",  
     output_format="mp3_44100_128",
     voice_settings={
         "stability": 0.3,
